5ebb39844a2e5228fff67945884f4cfad9bb89c7
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2022-09-15 09:01:31 -0600

    Tested timer

--- Assets/Scenes/Working Prototype/First Iteration Scripts/GameTimerProto/TimerDisplayTest.cs
+++ Assets/Scenes/Working Prototype/First Iteration Scripts/GameTimerProto/TimerDisplayTest.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using TMPro;
 
-namespace SRS.GameTimeProto;
+namespace SRS.GameTimeProto
 {
 	public class TimerDisplayTest : MonoBehaviour
 	{
 		private TMP_Text text;
+
+		private void Start()
+		{
+			text = GetComponent<TMP_Text>();	
+		}
+
 		void Update()
 		{
 			text.text = GameTimerProto.instance.currentTime.ToString();
