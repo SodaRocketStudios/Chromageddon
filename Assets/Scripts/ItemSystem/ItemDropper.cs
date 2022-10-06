7af566fc58b4816162af280d18b424de19146d06
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2022-10-06 08:21:59 -0600

    Created Item Dropper script

---
+++ Assets/Scripts/ItemSystem/ItemDropper.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace SRS.ItemSystem
+{
+	public class ItemDropper : MonoBehaviour
+	{
+		public static ItemDropper Instance;
+
+		[SerializeField]
+		private GameObject itemPickupPrefab;
+
+		private void Awake()
+		{
+			if(Instance == null)
+			{
+				Instance = this;
+			}
+			else if(Instance != this)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		public void DropItem(Vector2 position)
+		{
+			Instantiate(itemPickupPrefab, position, Quaternion.identity);
+		}
+	}
+}
