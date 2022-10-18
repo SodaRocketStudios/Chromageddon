d9bcccef6b41efbcdbd6e37000a30335d3b847cd
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2022-10-18 11:55:14 -0600

    remade roam state

--- Assets/Scripts/TopDownCharacter/AI/States/RoamState.cs
+++ Assets/Scripts/TopDownCharacter/AI/States/RoamState.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using System;
 
 namespace SRS.TopDownCharacterControl.AI
 {
 	public class RoamState : AIState
 	{
+        private const float MAX_DEVIATION = 10;
+        private System.Random random = new System.Random();
+
+        private Vector2 startPosition;
+
 		public override void Enter()
         {
-            throw new System.NotImplementedException();
+            // get start position
         }
 
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            Vector2 targetLocation = GetRandomLocation();
+        }
+
+        private Vector2 GetRandomLocation()
+        {
+            return startPosition + new Vector2(random.Next(), random.Next())*MAX_DEVIATION;
         }
 	}
 }
