83d1140b008c71613a37f3ae28aba1460cbb4c32
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2022-10-31 11:08:02 -0600

    fixed base character stats

--- Assets/Scripts/CharacterStatSystem/CharacterStats.cs
+++ Assets/Scripts/CharacterStatSystem/CharacterStats.cs
@@ -6,7 +6,7 @@ namespace SRS.StatSystem
 	public class CharacterStats : MonoBehaviour
 	{
 		[SerializeField] private BaseCharacterStats baseStats;
-
+		
 		public Dictionary<string, Stat> Character = new Dictionary<string, Stat>();
 		public Dictionary<string, Stat> Attack = new Dictionary<string, Stat>();
 
