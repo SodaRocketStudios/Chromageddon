6d784f7df6ca7c72f71701a4f6d7500a4fdf96e4
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2022-12-21 10:37:22 -0700

    Added volume adjustment to audio manager

--- Assets/Scripts/AudioManager/AudioManager.cs
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace SRS
 {
@@ -6,12 +7,17 @@ namespace SRS
 	{
 		public static AudioManager Instance;
 
+		[SerializeField] private AudioMixer mixer;
+
 		private AudioSource MusicSource;
 		private AudioSource EffectSource;
 
 		private void Awake()
 		{
 			Instance = this;
+
+			MusicSource.outputAudioMixerGroup = mixer.FindMatchingGroups("Music")[0];
+			EffectSource.outputAudioMixerGroup = mixer.FindMatchingGroups("Effects")[0];
 		}
 
 		public void PlayMusic(AudioClip clip)
@@ -23,6 +29,34 @@ namespace SRS
 		public void PlayEffect(AudioClip clip)
 		{
 			EffectSource.PlayOneShot(clip);
+		}
+
+		public void SetMasterVolume(float level)
+		{
+			level = LinearToDecibel(level);
+			mixer.SetFloat("VolumeMaster", level);
+		}
+
+		public void SetMusicVolume(float level)
+		{
+			level = LinearToDecibel(level);
+			mixer.SetFloat("VolumeMusic", level);
+		}
+
+		public void SeteffectsVolume(float level)
+		{
+			level = LinearToDecibel(level);
+			mixer.SetFloat("VolumeEffects", level);
+		}
+
+		private float LinearToDecibel(float value)
+		{
+			if(value <= 0)
+			{
+				return -80;
+			}
+
+			return 20*Mathf.Log10(value);
 		}
 	}
 }
