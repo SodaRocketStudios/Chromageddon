31acfee0b06be2119ea093b63589e24269560b4b
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-02-27 14:38:50 -0700

    Created sigmoid curve

---
+++ Assets/Scripts/Curves/SigmoidCurve.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace SRS.Curves
+{
+	public class SigmoidCurve
+	{
+		private float L;
+		private float x0;
+		private float y0;
+		private float k;
+
+		public float this[float x]
+		{
+			get
+			{
+				return L/(1 + Mathf.Exp(-k * (x - x0))) - y0;
+			}
+		}
+
+		public SigmoidCurve(float asymptote = 1, float horizontalShift = 0, float verticalShift = 0, float growthRate = 1)
+		{
+			this.L = asymptote;
+			this.x0 = horizontalShift;
+			this.y0 = verticalShift;
+			this.k = growthRate;
+		}
+	}
+}
