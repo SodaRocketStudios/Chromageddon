f6dfdfa44ef2a671d07a6f68b2d58b58a2c0d131
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-03-15 12:56:25 -0600

    Added mobile attack state

--- Assets/Scripts/TopDownCharacter/AI/MobileAttackState.cs
+++ Assets/Scripts/TopDownCharacter/AI/MobileAttackState.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 namespace SRS.TopDownCharacterControl.AI
 {
-    [CreateAssetMenu(fileName = "New Stationary Attack State", menuName = "AI/State/Stationary Attack State")]
-    public class StationaryAttackState : State
-    {
-        public override void Enter(AIBrain brain)
+	[CreateAssetMenu(fileName = "New Mobile Attack State", menuName = "AI/State/Mobile Attack State")]
+	public class MobileAttackState : State
+	{
+		public override void Enter(AIBrain brain)
         {
-			brain.StopMoving();
 			brain.AttackManager.IsAttacking = true;
         }
 
         public override void Execute(AIBrain brain)
         {
+			brain.MoveToward(brain.Target.position);
 			brain.LookAt(brain.Target.position);
         }
 
         public override void Exit(AIBrain brain)
         {
 			brain.AttackManager.IsAttacking = false;
+			brain.StopMoving();
         }
-    }
+	}
 }
