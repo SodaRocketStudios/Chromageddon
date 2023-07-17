263eb3ac8e3a14893f3200cd6759925f1958fd70
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-07-17 15:11:29 -0600

    Made death particles work when enemies die

--- Assets/Scripts/Particles/DeathParticles.cs
+++ Assets/Scripts/Particles/DeathParticles.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace SRS.Particles
 {
 	public class DeathParticles : MonoBehaviour
-	{
-		private ParticlePool pool;
+	{	
+		private ObjectPool<ParticleSystem> pool;
 
-		private void Start()
+		[SerializeField] private ParticleSystem particlePrefab;
+
+		private void Awake()
 		{
-			pool = GetComponent<ParticlePool>();	
+			pool = new ObjectPool<ParticleSystem>(Create, OnGet, OnRelease);
 		}
 
 		public void Play(GameObject target)
 		{
-			ParticleSystem particles = pool.Pool.Get();
+			ParticleSystem particles = pool.Get();
 			particles.transform.position = target.transform.position;
+			particles.startColor = target.GetComponent<SpriteRenderer>().color;
+			particles.Play();
+		}
+
+		
+		private ParticleSystem Create()
+		{
+			ParticleSystem newParticle = Instantiate(particlePrefab);
+			newParticle.gameObject.SetActive(false);
+
+			return newParticle;
+		}
+
+		private void OnGet(ParticleSystem particleSystem)
+		{
+			particleSystem.gameObject.SetActive(true);
+		}
+
+		private void OnRelease(ParticleSystem particleSystem)
+		{
+			particleSystem.gameObject.SetActive(false);
 		}
 	}
 }
