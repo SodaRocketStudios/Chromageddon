4921e564730b2805f8b843d7c1d6ca78ceedbdf5
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-08-14 13:04:34 -0600

    cached enemy spawn data in shop

--- Assets/Scripts/EnemySpawner/EnemyShop.cs
+++ Assets/Scripts/EnemySpawner/EnemyShop.cs
@@ -8,15 +8,20 @@ namespace SRS.EnemySpawner
     public class EnemyShop
     {
         private List<GameObject> enemyTypes = new();
+        private Dictionary<GameObject, SpawnData> spawnData = new();
 
         public EnemyShop(List<GameObject> enemyTypes)
         {
             this.enemyTypes = enemyTypes;
+            foreach(GameObject enemy in enemyTypes)
+            {
+                spawnData[enemy] = enemy.GetComponent<SpawnData>();
+            }
         }
 
         public GameObject GetEnemy(int points)
         {
-            return enemyTypes.Where(enemy => enemy.GetComponent<SpawnData>().Cost <= points).ToList().GetRandom();
+            return enemyTypes.Where(enemy => spawnData[enemy].Cost <= points).ToList().GetRandom();
         }
     }
 }
