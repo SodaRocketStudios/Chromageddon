6edebd32ef5a86cefa87c34d442173f1f29bda96
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-10-26 15:53:00 -0600

    Created fraction value util

---
+++ Assets/Scripts/Utils/Fraction.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SRS.Utils
+{
+	[Serializable]
+	public class Fraction
+	{
+		public UnityEvent<float> OnCurrentChange;
+		public UnityEvent<float> OnMaxChange;
+
+		private float current;
+		public float Current
+		{
+			get
+			{
+				return current;
+			}
+
+			set
+			{
+				current = Mathf.Max(value, 0);
+				current = Mathf.Min(current, Max);
+				OnCurrentChange?.Invoke(current);
+			}
+		}
+
+		private float max;
+		public float Max
+		{
+			get
+			{
+				return max;
+			}
+
+			set
+			{
+				max = Mathf.Max(value, 0);
+				OnMaxChange?.Invoke(max);
+			}
+		}
+	}
+}
