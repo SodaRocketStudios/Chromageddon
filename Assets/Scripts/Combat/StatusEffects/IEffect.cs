6f876b35120a9b61754285976c7af446d3579924
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-11-17 10:22:30 -0700

    Implemented status effects

--- Assets/Scripts/Combat/StatusEffects/IEffect.cs
+++ Assets/Scripts/Combat/StatusEffects/IEffect.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+using SRS.Stats;
 
 namespace SRS.Combat.StatusEffects
 {
-	public class IEffect : MonoBehaviour
+	public interface IEffect
 	{
-		
+		public void Apply(StatContainer targetStats);
+
+		public void Remove(StatContainer targetStats);
 	}
 }
--- Assets/Scripts/Combat/StatusEffects/StatusEffect.cs
+++ Assets/Scripts/Combat/StatusEffects/StatusEffect.cs
@@ -1,9 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
+using SRS.Stats;
 
 namespace SRS.Combat.StatusEffects
 {
-	public class StatusEffect : MonoBehaviour
-	{
-		
-	}
+    public class StatusEffect : IOnHitEffect
+    {
+		[SerializeField] private List<IEffect> effects;
+
+        public void Trigger(StatContainer targetStats)
+        {
+            Apply(targetStats);
+        }
+
+		public void Apply(StatContainer targetStats)
+		{
+			foreach(IEffect effect in effects)
+			{
+				effect.Apply(targetStats);
+			}
+		}
+
+		public void Remove(StatContainer targetStats)
+		{
+			foreach(IEffect effect in effects)
+			{
+				effect.Remove(targetStats);
+			}
+		}
+    }
 }
