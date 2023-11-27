4e2f958e57b27fef84168b250c73a8b31131819d
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-11-27 11:31:08 -0700

    updated unity version

--- Assets/Scripts/Combat/StatusEffects/TickDamageEffect.cs
+++ Assets/Scripts/Combat/StatusEffects/TickDamageEffect.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using SRS.Stats;
 using UnityEngine;
 
@@ -12,15 +13,28 @@ namespace SRS.Combat.StatusEffects
 
 		private HitHandler targetHitHandler;
 
+        private bool isRunning = false;
+
         public void Apply(StatContainer targetStats)
         {
 			tickDelay = 1/tickRate;
+            isRunning = true;
+            Tick();
 			// TODO -- Start Effect coroutine;
         }
 
         public void Remove(StatContainer targetStats)
         {
 			// TODO -- End Effect Coroutine;
+        }
+
+        private async Task Tick()
+        {
+            while()
+            {
+                Awaitable
+                targetHitHandler.Hit(DamageType.Fire);
+            }
         }
     }
 }
