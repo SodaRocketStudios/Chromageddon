86f5064185c08c602c36c51a5a0b01f48a0da59c
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2023-11-28 10:28:55 -0700

    implemented stat effects

--- Assets/Scripts/Combat/StatusEffects/StatEffect.cs
+++ Assets/Scripts/Combat/StatusEffects/StatEffect.cs
@@ -1,17 +1,20 @@
+using UnityEngine;
 using SRS.Stats;
 
 namespace SRS.Combat.StatusEffects
 {
     public class StatEffect : IEffect
     {
+        [SerializeField] private StatModifier statModifier;
+
         public void Apply(StatContainer targetStats)
         {
-            
+            statModifier.Apply(targetStats);
         }
 
         public void Remove(StatContainer targetStats)
         {
-            
+            statModifier.Remove(targetStats);
         }
     }
 }
