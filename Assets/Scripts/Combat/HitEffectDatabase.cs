e8d5ee12edce4eb6e7c686253076af5d7cf9b3e7
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-01-04 10:20:34 -0700

    removed static from hit effect list

--- Assets/Scripts/Combat/HitEffectDatabase.cs
+++ Assets/Scripts/Combat/HitEffectDatabase.cs
@@ -6,7 +6,7 @@ namespace SRS.Combat
 	[CreateAssetMenu(fileName = "New Hit Effect Database", menuName = "Combat/Hit Effects/Hit Effect Database")]
 	public class HitEffectDatabase : ScriptableObject
 	{
-		[SerializeField] private static List<IOnHitEffect> effects;
+		[SerializeField] private List<IOnHitEffect> effects;
 		public List<IOnHitEffect> Effects
 		{
 			get => effects;
