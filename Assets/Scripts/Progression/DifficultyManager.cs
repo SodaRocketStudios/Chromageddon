76fe01f65739ac57a0f65db077645455fd259156
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-01-04 15:48:44 -0700

    added challenge rating to difficulty manager

--- Assets/Scripts/Progression/DifficultyManager.cs
+++ Assets/Scripts/Progression/DifficultyManager.cs
@@ -4,6 +4,7 @@ namespace SRS.Progression
 {
 	public class DifficultyManager : MonoBehaviour
 	{
-		
+		private float challengeRating = 0;
+		public float ChallengeRating{ get => challengeRating; }
 	}
 }
