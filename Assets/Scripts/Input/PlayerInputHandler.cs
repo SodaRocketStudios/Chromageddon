35c303616279577336c9018907d9f3bf91c049e5
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-01-11 10:02:14 -0700

    added a way to disable player controls

--- Assets/Scripts/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Input/PlayerInputHandler.cs
@@ -6,13 +6,52 @@ namespace SRS.Input
     public class PlayerInputHandler : MonoBehaviour, IInputSource
     {
         private Vector2 moveInput;
-        public Vector2 MoveInput => moveInput;
+        public Vector2 MoveInput
+        {
+            get
+            {
+                if(MovementEnabled)
+                {
+                    return moveInput;
+                }
+                
+                return Vector2.zero;
+            }
+        }
 
         private Vector2 lookInput;
-        public Vector2 LookInput  => lookInput;
+        public Vector2 LookInput
+        {
+            get
+            {
+                if(LookEnabled)
+                {
+                    return lookInput;
+                }
+                
+                return Vector2.zero;
+            }
+        }
 
         private bool attackInput;
-        public bool AttackInput => attackInput;
+        public bool AttackInput
+        {
+            get
+            {
+                if(AttackEnabled)
+                {
+                    return attackInput;
+                }
+
+                return false;
+            }
+        }
+
+        public bool MovementEnabled{get; set;}
+
+        public bool LookEnabled{get; set;}
+
+        public bool AttackEnabled{get; set;}
 
         private bool IsUsingMouse;
 
@@ -21,6 +60,13 @@ namespace SRS.Input
         private void Start()
         {
             mainCamera = Camera.main;
+        }
+
+        public void SetControlsActive(bool isActive)
+        {
+            MovementEnabled = isActive;
+            LookEnabled = isActive;
+            AttackEnabled = isActive;
         }
 
         public void OnSchemeChange(PlayerInput input)
