2602f5d2f88ff75daecabf406043bd03dff63031
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-01-18 11:11:57 -0700

    removed unused code from xp merger

--- Assets/Scripts/Progression/XPMerger.cs
+++ Assets/Scripts/Progression/XPMerger.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
-using SRS.Extensions.Vector;
 
 namespace SRS.Progression
 {
@@ -9,35 +7,6 @@ namespace SRS.Progression
 	public class XPMerger
 	{
 		[SerializeField] private float mergeRange;
-
-		// public bool TryMerge(List<Experience> activePickups)
-		// {
-		// 	bool mergeSucceeded = false;
-
-		// 	foreach(Experience pickup in activePickups)
-		// 	{
-		// 		RaycastHit2D[] hits = Physics2D.CircleCastAll(pickup.transform.position, mergeRange, Vector2.right, 0, LayerMask.GetMask("XP"));
-
-		// 		List<Vector3> locations = new()
-        //         {
-        //             pickup.transform.position
-        //         };
-
-		// 		foreach(RaycastHit2D hit in hits)
-		// 		{
-		// 			locations.Add(hit.transform.position);
-		// 		}
-
-		// 		Vector3 centroid = VectorExtensions.Average(locations);
-
-		// 		foreach(RaycastHit2D hit in hits)
-		// 		{
-		// 			hit.transform.GetComponent<Experience>().Merge(pickup);
-		// 		}
-		// 	}
-
-		// 	return mergeSucceeded;
-		// }
 
 		public int TryMerge(Vector3 mergeLocation)
 		{
