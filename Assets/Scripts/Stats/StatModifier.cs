750c090ab85f0b66e7acc716292cf5b98680c3b0
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-01-31 10:03:54 -0700

    removed descriptions from stat modifiers and made other data readable

--- Assets/Scripts/Stats/StatModifier.cs
+++ Assets/Scripts/Stats/StatModifier.cs
@@ -5,18 +5,19 @@ namespace SRS.Stats
     public abstract class StatModifier: ScriptableObject
     {
         [SerializeField] protected string affectedStat;
+        public string AffectedStat
+        {
+            get => affectedStat;
+        }
 
         [SerializeField] protected float value;
-
-        [SerializeField] private string description;
+        public float Value
+        {
+            get => value;
+        }
 
         public abstract void Apply(StatContainer container);
 
         public abstract void Remove(StatContainer container);
-
-        public string GetFormattedDescription()
-        {
-            return "";
-        }
     }
 }
