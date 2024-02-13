e8b0f84cab96e4a8237ec710ea84324395eefe09
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-02-13 05:37:35 -0700

    implemented knockbackzone

---
+++ Assets/Scripts/PawnController/KnockbackZone.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace SRS.PawnController
+{
+	public class KnockbackZone : MonoBehaviour
+	{
+		[SerializeField] private float magnitude;
+
+		[SerializeField] private float range;
+
+		[SerializeField] private LayerMask collisionMask;
+
+		public void Trigger()
+		{
+			Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, range, collisionMask);
+
+			foreach(Collider2D collider in colliders)
+			{
+				Vector2 direction = (transform.position - collider.transform.position).normalized;
+				collider.GetComponent<PawnMover>()?.AddVelocity(direction*magnitude);
+			}
+		}
+	}
+}
