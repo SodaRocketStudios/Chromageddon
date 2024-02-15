a8146797ab674aa3ce0fb05e93133030018b2d35
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-02-15 08:48:08 -0700

    first attempt at wander action

--- Assets/Scripts/AI/Actions/Wander.cs
+++ Assets/Scripts/AI/Actions/Wander.cs
@@ -1,13 +1,26 @@
+using System;
 using UnityEngine;
+using SRS.Extensions.Random;
 
 namespace SRS.AI
 {
 	[CreateAssetMenu(fileName = "New Wander Action", menuName = "Enemies/AI/Actions/Wander Action")]
     public class Wander : Action
     {
-        public override void Execute(AIBrain brain)
+		[SerializeField] private float wanderRange;
+
+		[SerializeField] private float tolerance;
+
+	 	private System.Random random = new(Guid.NewGuid().GetHashCode());
+
+		public override void Execute(AIBrain brain)
         {
-            brain.MoveInput = brain.Player.position - brain.transform.position;
+			if(brain.TargetDistanceSquared <= tolerance || brain.Target == Vector2.zero)
+			{
+				brain.Target = random.WithinUnitCircle()*wanderRange;
+			}
+			
+            brain.MoveInput = brain.Target - (Vector2)brain.transform.position;
         }
     }
 }
