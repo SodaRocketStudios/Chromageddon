52b4f1eb42cbb5aecd0bff0e0184d37a73e3b6cf
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-02-21 09:12:31 -0700

    created aim guide

--- Assets/Scripts/Combat/AimGuide.cs
+++ Assets/Scripts/Combat/AimGuide.cs
@@ -1,9 +1,27 @@
+using SRS.Stats;
 using UnityEngine;
 
 namespace SRS.Combat
 {
 	public class AimGuide : MonoBehaviour
 	{
-		
+		private LineRenderer lineRenderer;
+
+		private StatContainer playerStats;
+
+		private void Awake()
+		{
+			lineRenderer = GetComponent<LineRenderer>();
+
+			playerStats = GetComponent<StatContainer>();
+
+			lineRenderer.positionCount = 2;
+		}
+
+		private void Update()
+		{
+			lineRenderer.SetPosition(0, transform.position);
+			lineRenderer.SetPosition(1, transform.right*playerStats["Range"].Value);
+		}
 	}
 }
