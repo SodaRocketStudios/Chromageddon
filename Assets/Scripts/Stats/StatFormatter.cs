8afcde7fc032405826b145a9397a4fff7bbc0f32
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-02-22 08:18:01 -0700

    added formatting to stat formatter

--- Assets/Scripts/Stats/StatFormatter.cs
+++ Assets/Scripts/Stats/StatFormatter.cs
@@ -4,11 +4,27 @@ namespace SRS.Stats
 	{
 		public static string GetStat(StatContainer stats, string statName)
 		{
-			string stat;
+			string statData;
 
-			stat = $"{statName}: {stats[statName].BaseValue:0.#} x {stats[statName].PercentageModifier*100:I}";
+			Stat stat = stats[statName];
 
-			return stat;
+			switch(stat.Format)
+			{
+				case StatFormat.Full:
+					statData = $"{statName}: {stat.BaseValue:0.0} x {stat.PercentageModifier*100:I} = {stat.Value}";
+					break;
+				case StatFormat.BaseValue:
+					statData = $"";
+					break;
+				case StatFormat.Percentage:
+					statData = $"";
+					break;
+				default:
+					statData = "No format set";
+					break;
+			}
+
+			return statData;
 		}
 	}
 
