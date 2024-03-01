c3f7c06b016c272772fec3a3e07e473aeb412f29
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-03-01 09:56:27 -0700

    implemented health regen

--- Assets/Scripts/Combat/HealthRegen.cs
+++ Assets/Scripts/Combat/HealthRegen.cs
@@ -1,9 +1,61 @@
+using System.Threading;
 using UnityEngine;
+using SRS.Stats;
 
 namespace SRS.Combat
 {
 	public class HealthRegen : MonoBehaviour
 	{
+		[SerializeField] private float regenMultiplier;
+
+		[SerializeField] private float regenInterval;
+
+		private Health health;
+
+		private StatContainer stats;
+
+		private CancellationTokenSource tokenSource = new();
 		
+		private float timer = 0;
+
+		private void Awake()
+		{
+			health = GetComponent<Health>();
+			stats = GetComponent<StatContainer>();
+		}
+
+		private void OnEnable()
+		{
+			tokenSource.Dispose();
+			tokenSource = new();
+
+			Regen(tokenSource.Token);
+		}
+
+		private void OnDisable()
+		{
+			tokenSource.Cancel();
+		}
+
+		private async void Regen(CancellationToken token)
+		{
+			while(health.Value.Current > 0)
+			{
+				if(token.IsCancellationRequested)
+				{
+					break;
+				}
+
+				if(timer >= regenInterval)
+				{
+					health.Heal(stats["Health Regen"].Value*regenMultiplier);
+					timer = 0;
+				}
+
+				timer += Time.deltaTime;
+
+				await Awaitable.NextFrameAsync();
+			}
+		}
 	}
 }
