a6f6b0720905be6fced56112cda15d546ec567cf
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-03-12 11:12:37 -0600

    added achievement display

--- Assets/Scripts/UI/AchievementDisplay.cs
+++ Assets/Scripts/UI/AchievementDisplay.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
 using SRS.Achievements;
-using System;
+using TMPro;
 
 namespace SRS.UI
 {
 	public class AchievementDisplay : MonoBehaviour
 	{
-		private void Start()
+		[SerializeField] private TMP_Text NameTextBox;
+		[SerializeField] private TMP_Text DescriptionTextBox;
+
+		private void OnEnable()
 		{
 			Achievement.AchievementAwarded += Show;
 		}
 
+		private void OnDisable()
+		{
+			Achievement.AchievementAwarded -= Show;
+		}
+
         private void Show(Achievement achievement)
         {
-			Debug.Log(achievement.Description);
+			NameTextBox.text = achievement.name;
+			DescriptionTextBox.text = achievement.Description;
+			PlayAnimation();
         }
+
+		private async void PlayAnimation()
+		{
+			await Awaitable.NextFrameAsync();
+		}
     }
 }
