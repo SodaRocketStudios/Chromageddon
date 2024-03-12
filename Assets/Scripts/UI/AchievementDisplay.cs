fba02f85306572fa8b48b265e05907f8e92e30fa
renckbryce <87397393+SodaRocketStudios@users.noreply.github.com>
2024-03-12 11:45:25 -0600

    initial animation of achievement display

--- Assets/Scripts/UI/AchievementDisplay.cs
+++ Assets/Scripts/UI/AchievementDisplay.cs
@@ -9,6 +9,27 @@ namespace SRS.UI
 		[SerializeField] private TMP_Text NameTextBox;
 		[SerializeField] private TMP_Text DescriptionTextBox;
 
+		[SerializeField] private float travelTime;
+		[SerializeField] private float dwellTime;
+
+		private Vector3 startPosition;
+		private Vector3 showPosition;
+
+		private float timer = 0;
+
+		private RectTransform rectTransform;
+
+		private void Awake()
+		{
+			rectTransform = GetComponent<RectTransform>();
+		}
+
+		private void Start()
+		{
+			startPosition = rectTransform.position;
+			showPosition = Vector3.down*(GetComponentInParent<RectTransform>().sizeDelta.y - rectTransform.sizeDelta.y);
+		}
+
 		private void OnEnable()
 		{
 			Achievement.AchievementAwarded += Show;
@@ -28,7 +49,35 @@ namespace SRS.UI
 
 		private async void PlayAnimation()
 		{
-			await Awaitable.NextFrameAsync();
+			timer = 0;
+
+			float t;
+
+			while(timer < travelTime)
+			{
+				timer += Time.deltaTime;
+				t = timer / travelTime;
+				rectTransform.position = Vector3.Lerp(startPosition, showPosition, t);
+				await Awaitable.NextFrameAsync();
+			}
+
+			timer = 0;
+
+			while(timer < dwellTime)
+			{
+				timer += Time.deltaTime;
+				await Awaitable.NextFrameAsync();
+			}
+
+			timer = 0;
+
+			while(timer < travelTime)
+			{
+				timer += Time.deltaTime;
+				t = timer / travelTime;
+				rectTransform.position = Vector3.Lerp(showPosition, startPosition, t);
+				await Awaitable.NextFrameAsync();
+			}
 		}
     }
 }
